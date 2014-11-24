071888e00c8fb0647639b07e389787a6c18a94db
Wojtek Krysiak <wojciech.krysiak@kmpgroup.pl>
2014-11-25 00:02:25 +0100

    android icons and typo

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'whatbugsme',
+  description: 'whatbugsme',
+  version: "0.0.1"
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
